6d200b9e0d9280c36f6a9fbdbd443e810eb9e87b
Milan Camus <pro@turtletv.fr>
2022-06-01 18:11:40 +0200

    feat: ✨ Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import { Colors } from "../constants/Colors";
+import Svg, { Defs, ClipPath, Path, G, Rect } from "react-native-svg";
+import { hp, textColor, wp } from "@utils/functions";
+
+interface ButtonProps {
+  width?: number;
+  height?: number;
+  color: string;
+  children: string;
+}
+
+export const Button: React.FunctionComponent<ButtonProps> = ({
+  width,
+  height,
+  color,
+  children,
+}) => (
+  <TouchableOpacity
+    style={{
+      alignSelf: "center",
+    }}
+  >
+    <Svg width={width ?? wp("80%")} height={height ?? hp("7%")}>
+      <Defs>
+        <ClipPath id="a">
+          <Path fill="none" d="M0 0h375v100H0z" />
+        </ClipPath>
+      </Defs>
+      <G>
+        <G>
+          <Rect
+            data-name="bg"
+            width={width ?? wp("80%")}
+            height={height ?? hp("7%")}
+            rx={height ? height / 4 : hp("7%") / 4}
+            fill={color}
+          />
+        </G>
+      </G>
+    </Svg>
+    <View
+      style={{
+        position: "relative",
+        top: height ? -height : -hp("7%"),
+        marginBottom: height ? -height : -hp("7%"),
+        height: height ?? hp("7%"),
+        paddingHorizontal: hp("2%"),
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: hp("2.3%"),
+          color: textColor(color),
+          fontFamily: "Gibson",
+          fontWeight: "500",
+        }}
+      >
+        {children}
+      </Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({});
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,3 +2,11 @@ export {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+
+export const textColor = (bgColor: string) => {
+  var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16);
+  var g = parseInt(color.substring(2, 4), 16);
+  var b = parseInt(color.substring(4, 6), 16);
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "black" : "white";
+};
