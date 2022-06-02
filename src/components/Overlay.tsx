972d1c35abb859504911530d82c16200916a0de1
Milan Camus <pro@turtletv.fr>
2022-06-02 09:00:27 +0200

    feat: ✨ Add overlay component

---
+++ src/components/Overlay.tsx
@@ -0,0 +1,39 @@
+import { hp, wp } from "@utils/functions";
+import * as React from "react";
+import { ImageSourcePropType, View, Image } from "react-native";
+import { Colors } from "../constants/Colors";
+
+interface OverlayProps {
+  color?: string;
+  opacity?: number;
+  image?: ImageSourcePropType;
+}
+
+export const Overlay: React.FunctionComponent<OverlayProps> = ({
+  color,
+  opacity,
+  image,
+}) => (
+  <>
+    {image && (
+      <Image
+        source={image}
+        blurRadius={3}
+        style={{
+          width: wp("100%"),
+          height: hp("100%"),
+          position: "absolute",
+        }}
+      />
+    )}
+    <View
+      style={{
+        width: wp("100%"),
+        height: hp("100%"),
+        position: "absolute",
+        backgroundColor: color ?? Colors.black,
+        opacity: opacity ?? 0.3,
+      }}
+    ></View>
+  </>
+);
