dfdd6965c0a0531a3f6a16936fb822445c3837ec
Milan Camus <pro@turtletv.fr>
2022-06-02 08:24:35 +0200

    feat: ✨ Add new helper

---
+++ src/constants/TextsSizes.ts
@@ -0,0 +1,11 @@
+import { hp } from "@utils/functions";
+
+export const texts = {
+  title: hp("4%"),
+  subtitle: hp("2.75%"),
+  paragraph: hp("2%"),
+  small: hp("1.5%"),
+  tiny: hp("1%"),
+  input: hp("2%"),
+  button: hp("2.3%"),
+};
