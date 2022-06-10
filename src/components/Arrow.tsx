d6781fc8dc695d600b718fc6ab4120b7434ddcdf
Milan Camus <pro@turtletv.fr>
2022-06-11 01:10:56 +0200

    feat: ✨ Add new arrow component

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,60 @@
+import { hp } from "@utils/functions";
+import * as React from "react";
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Colors } from "../constants/Colors";
+
+interface ArrowProps {
+  onPress: () => void;
+  color?: string;
+  size?: number;
+  direction?: "left" | "right" | "up" | "down";
+  top?: number;
+  left?: number;
+}
+
+export const Arrow: React.FunctionComponent<ArrowProps> = ({
+  onPress,
+  color,
+  size,
+  direction,
+  top,
+  left,
+}) => (
+  <TouchableOpacity
+    onPress={() => onPress()}
+    style={{
+      position: "absolute",
+      top: top ?? hp("5%"),
+      left: left ?? hp("3%"),
+      width: size ?? hp("4%"),
+      height: size ?? hp("4%"),
+      zIndex: 99999,
+    }}
+  >
+    <Image
+      style={{
+        width: size ?? hp("4%"),
+        height: size ?? hp("4%"),
+        tintColor: color ?? Colors.white,
+        transform: [
+          {
+            rotate:
+              direction === "right"
+                ? "180deg"
+                : direction === "up"
+                ? "90deg"
+                : direction === "down"
+                ? "270deg"
+                : "0deg",
+          },
+        ],
+      }}
+      source={require("@images/arrow.png")}
+      width={size ?? hp("4%")}
+      height={size ?? hp("4%")}
+      resizeMode="contain"
+    />
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({});
