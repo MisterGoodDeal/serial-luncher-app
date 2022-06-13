e6b71179f40d1a9778282e3ca29646e5131dd7a6
Milan Camus <pro@turtletv.fr>
2022-06-13 19:22:27 +0200

    feat: ⚰️ Remove old loader

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import useCachedResources from "./src/hooks/useCachedResources";
 import { RootNavigator } from "./src/navigation/RootNavigator";
 import { store } from "./src/store/store";
 import GenericModal from "@components/GenericModal";
-import { Loader } from "@components/Loader";
 import Toast from "react-native-toast-message";
 
 export default function App() {
@@ -17,7 +16,6 @@ export default function App() {
       <>
         <Provider store={store}>
           <GenericModal />
-          <Loader />
           <RootNavigator />
         </Provider>
         <Toast />
