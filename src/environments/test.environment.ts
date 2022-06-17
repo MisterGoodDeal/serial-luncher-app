fd5f4d4b55e72c83d5bacd4dab518d3d6a4c7cc6
Milan Camus <pro@turtletv.fr>
2022-06-17 17:56:48 +0200

    feat: ✨ Add `serial-luncher-server` url

--- src/environments/test.environment.ts
+++ src/environments/test.environment.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://pokeapi.co/api/v2/";
-export const SERIAL_LUNCHER_API = "http://192.168.1.88:3000/";
+export const SERIAL_LUNCHER_API = "https://serial-luncher-api.turtlecorp.fr";
